78c1f393b054201ccac40d0178fff6ed9da72ae4
Your Name <your.email@example.com>
2025-09-06 16:38:15 +0800

    Initial Commit

---
+++ ichat_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ichat_app
+//
+//  Created by Max on 6/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
